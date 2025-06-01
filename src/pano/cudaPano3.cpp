59d03f9109abdacada8675591af07b8c0bb9f0ee
Chris Olivier <cjolivier01@apache.org>
2025-06-01 07:24:39 -0700

    ...

--- src/pano/cudaPano3.cpp
+++ src/pano/cudaPano3.cpp
@@ -3,9 +3,5 @@
 namespace hm {
 namespace pano {
 
-/**
- * Nothing to do in this .cpp—template definitions and .inl already cover everything
- * (just here for completeness).
- */
 } // namespace pano
 } // namespace hm
